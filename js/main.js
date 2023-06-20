8affbc01fde678a641e1dfeae36eb097a2bf55e2
Fernando Milani <milani.fernando98@gmail.com>
2023-06-19 23:42:48 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,121 @@
+// PRODUCTOS
+const productos = [
+    // Volantes
+    {
+        id: "MOMO",
+        titulo: "Volante MOMO",
+        imagen: "./img/MOMO R1909.jpg",
+        categoria: {
+            nombre: "Volante",
+            id: "volante"
+        },
+        precio: 60000
+    },
+    {
+        id: "VERTEX",
+        titulo: "Volante VERTEX KUMADORI",
+        imagen: "./img/VERTEX KUMADORI.jpg",
+        categoria: {
+            nombre: "Volante",
+            id: "volante"
+        },
+        precio: 180000
+    },
+    // Hi-Flow Exhausts
+    {
+        id: "HKS",
+        titulo: "Escape HKS Hi-Power",
+        imagen: "./img/hks hipower.jpg",
+        categoria: {
+            nombre: "Hi-Flow Exhausts",
+            id: "hi-flow exhausts"
+        },
+        precio: 220000
+    },
+    {
+        id: "Greddy",
+        titulo: "Greddy Revolution RS",
+        imagen: "./img/Greddy Revolution RS.jpg",
+        categoria: {
+            nombre: "HKS Hi-Flow Exhausts",
+            id: "hi-flow exhausts"
+        },
+        precio: 160000
+    },
+    // Ruedas y Neumaticos
+    {
+        id: "RS Watanabe",
+        titulo: "Llanta RS Watanabe R17",
+        imagen: "./img/RS-Watanabe.jpg",
+        categoria: {
+            nombre: "Ruedas y Neumaticos",
+            id: "ruedas y neumaticos"
+        },
+        precio: 240000
+    },
+    {
+        id: "Toyo Proxes",
+        titulo: "Neumatico Toyo Proxes",
+        imagen: "./img/Toyo Tires.jpg",
+        categoria: {
+            nombre: "Ruedas y Neumaticos",
+            id: "ruedas y neumaticos"
+        },
+        precio: 150000
+    },
+    {
+        id: "Desmond EVO",
+        titulo: "Llanta Desmond EVO Type R",
+        imagen: "./img/Desmon EVO Type R.jpg",
+        categoria: {
+            nombre: "Ruedas y Neumaticos",
+            id: "ruedas y neumaticos"
+        },
+        precio: 850000
+    },
+    // Rendimiento
+    {
+        id: "Custom Fuel Injection",
+        titulo: "Rampa de Inyeccion Custom Fuel Injection Rail 90MM",
+        imagen: "./img/Fuel Injection Rail.jpg",
+        categoria: {
+            nombre: "Rendimiento",
+            id: "rendimiento"
+        },
+        precio: 200000 
+    },
+    {
+        id: "Skunk2",
+        titulo: "Multiple Escape Skunk2 Racing",
+        imagen: "./img/Multiple Skunk2.jpg",
+        categoria: {
+            nombre: "Rendimiento",
+            id: "rendimiento"
+        },
+        precio: 260000 
+    },
+    //Decoracion y Estetica
+    {
+        id: "Aero Kit",
+        titulo: "Aero Kit Rocket Bunny Nissan",
+        imagen: "./img/rocket bunny.jpg",
+        categoria: {
+            nombre: "Rendimiento",
+            id: "rendimiento"
+        },
+        precio: 800000 
+
+    },
+    {
+        id: "IMPUL",
+        titulo: "Paragolpe IMPUL Nissan GTR",
+        imagen: "./img/IMPUL kit.jpg",
+        categoria: {
+            nombre: "Rendimiento",
+            id: "rendimiento"
+        },
+        precio: 800000  
+    },
+];
+
+function cargarProductos{} {}
